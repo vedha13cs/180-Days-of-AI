cfa587e944f078a7cb0e0c58acf8b634202d1be2
Vedhavathi Y A <vedha9782@gmail.com>
2026-08-15 23:29:08 +0530

    Add AI Learning Tracker for Day 1

---
+++ Day1/AI-Learning-Tracker.py
@@ -0,0 +1,16 @@
+AI Learning Tracker - Day 1
+
+name = input("Enter your name: ")
+hours = float(input("How many hours did you study AI today? "))
+
+print("\n==============================")
+print("      AI LEARNING PROGRESS")
+print("==============================")
+
+print("Name:", name)
+print("Day:", 1)
+print("Study Hours:", hours)
+print("Status: Started!")
+
+print("\n🚀 Day 1 completed!")
+print("Keep learning. Keep building.")
