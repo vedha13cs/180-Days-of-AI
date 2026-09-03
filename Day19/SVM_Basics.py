67b846eb697dd965020726a982990010aaca587f
Vedhavathi Y A <vedha9782@gmail.com>
2026-09-03 23:41:30 +0530

    Update SVM_Basics.py

--- Day19/SVM_Basics.py
+++ Day19/SVM_Basics.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score 
 
 print("⚡ SUPPORT VECTOR MACHINE")
 print("=" * 50)
